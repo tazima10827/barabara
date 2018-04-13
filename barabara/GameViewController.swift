de1ddf965bf27ed94579332bee61853816637f40
Motohiro Tajima <kapiha7@gmail.com>
2018-04-13 23:36:19 +0900

    Errorを直しました。

--- barabara/GameViewController.swift
+++ barabara/GameViewController.swift
@@ -18,9 +18,9 @@ class GameViewController: UIViewController {
     var timer: Timer!
     var score: Int = 1000
     let defaults:UserDefaults = UserDefaults.standard
-    let width:  CGFont = UIScreen.main.bounds.size.width as! CGFont
-    var positionX: [CGFont] = [0.0 as! CGFont,0.0 as! CGFont,0.0 as! CGFont]
-    var dx: [CGFont] = [1.0 as! CGFont,0.5 as! CGFont,-1.0 as! CGFont]
+    let width:  CGFloat = UIScreen.main.bounds.size.width
+    var positionX: [CGFloat] = [0.0,0.0,0.0]
+    var dx: [CGFloat] = [1.0,0.5,-1.0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,14 +33,16 @@ class GameViewController: UIViewController {
     
     @objc func up(){
         for i in 0..<3 {
-            if positionX[i] > width || positionXpositionX[i] < 0 {
+            if positionX[i] > width || positionX[i] < 0 {
                 dx[i] = dx[i] * -1
             }
             positionX[i] += dx[i]
         }
-        imageView1.center.x = positionX[0] as! CGFloat
-        imageVIew2.center.x = positionX[1] as! CGFloat
+        imageView1.center.x = positionX[0]
+        imageVIew2.center.x = positionX[1]
+        imageView3.center.x = positionX[2]
     }
+    
     func start(){
         resultLabel.isHidden = true
         timer = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(self.up),userInfo: nil, repeats: true)
@@ -50,7 +52,28 @@ class GameViewController: UIViewController {
     @IBAction func stop(_ sender: Any) {
         if timer.isValid == true {
             timer.invalidate()
+            
+            for i in 0..<3 {
+                score = score - abs(Int(width/2 - positionX[i]))*2
+            }
+            resultLabel.text = "Score: " + String(score)
+            resultLabel.isHidden = false
         }
+        let highScore1: Int = defaults.integer(forKey: "score1")
+        let highScore2: Int = defaults.integer(forKey: "score2")
+        let highScore3: Int = defaults.integer(forKey: "score3")
+        if score > highScore1 {
+            defaults.set(score,forKey: "score1")
+            defaults.set(highScore1,forKey: "score2")
+            defaults.set(highScore2,forKey: "score3")
+        }else if score > highScore2 {
+            defaults.set(score,forKey: "score2")
+            defaults.set(highScore2,forKey: "score3")
+            
+        }else if score > highScore3 {
+            defaults.set(score,forKey: "score3")
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -58,7 +81,17 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func retry(_ sender: Any) {
+        score = 1000 // スコアの値をリセットする
+        positionX = [width/2,width/2,width/2] //画像の位置を真ん中に戻す
+        if timer.isValid == false {
+            self.start() //スタートメソッドを呼び出す
+        }
+    }
+    
+    @IBAction func goTop(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
--- barabara/RankingViewController.swift
+++ barabara/RankingViewController.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 class RankingViewController: UIViewController {
+    
+    
+    @IBOutlet weak var rankingLabel1: UILabel!
+    @IBOutlet weak var rankingLabel2: UILabel!
+    @IBOutlet weak var rankingLabel3: UILabel!
+    
+    let defaults: UserDefaults = UserDefaults.standard
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        rankingLabel1.text = String(defaults.integer(forKey: "score1"))
+        rankingLabel2.text = String(defaults.integer(forKey: "score2"))
+        rankingLabel3.text = String(defaults.integer(forKey: "score3"))
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func goTop(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
